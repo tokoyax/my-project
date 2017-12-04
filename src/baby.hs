a144621cd3661c3edea211e7c171e6860f358737
tokoyax <tokoyax.dev@gmail.com>
2017-12-04 17:25:56 +0900

    page 16

--- src/baby.hs
+++ src/baby.hs
@@ -7,3 +7,5 @@ doubleSmallNumber x = if x > 100
                         else x * 2
 
 doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
+
+boomBangs xs = [ if x < 10 then "BOOM!!!" else "BANG!!!" | x <- xs, odd x]
