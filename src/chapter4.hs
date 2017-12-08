141ee455b72b9be19917ccbe20172e0cd4aa3cc1
tokoyax <tokoyax.dev@gmail.com>
2017-12-08 15:41:54 +0900

    page 56

---
+++ src/chapter4.hs
@@ -0,0 +1,38 @@
+-- p52
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list!"
+maximum' [x] = x
+maximum' (x:xs) = max x (maximum' xs)
+
+-- p53
+replicate' :: Int -> a -> [a]
+replicate' n x
+  | n <= 0    = []
+  | otherwise = x : replicate' (n - 1) x
+
+take' :: Int -> [a] -> [a]
+take' n _
+  | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n - 1) xs
+-- p55
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x : repeat' x
+
+-- p56
+zip' :: [a] -> [a] -> [(a, a)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x, y) : zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+  | a == x    = True
+  | otherwise = a `elem'` xs
+
+
