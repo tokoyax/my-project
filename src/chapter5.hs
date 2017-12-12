3dae55388b831db4bd1ad00468200d73008d5b0b
tokoyax <tokoyax.dev@gmail.com>
2017-12-12 12:52:03 +0900

    chapter 5

--- src/chapter5.hs
+++ src/chapter5.hs
@@ -9,3 +9,17 @@ compareWithHundred = compare 100
 -- p64
 divideByTen :: (Floating a) => a -> a
 divideByTen = (/10)
+
+-- p65
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+-- p66
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = [] -- リスト a が空
+zipWith' _ _ [] = [] -- リスト b が空
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+-- p67
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f y x = f x y
