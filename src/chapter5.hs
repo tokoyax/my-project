2b127c83468c753d1f27856c1fde4a2c561d42cc
tokoyax <tokoyax.dev@gmail.com>
2017-12-19 00:07:33 +0900

    foldl1, foldr1

--- src/chapter5.hs
+++ src/chapter5.hs
@@ -22,19 +22,19 @@ zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 
 -- p67
 flip' :: (a -> b -> c) -> b -> a -> c
-flip' f = \x y -> f x y
+flip' f = \x y -> f y x
 
 -- p68
-map' :: (a -> b) -> [a] -> [b]
-map' _ [] = []
-map' f (x:xs) = f x : map' f xs
+--map' :: (a -> b) -> [a] -> [b]
+--map' _ [] = []
+--map' f (x:xs) = f x : map' f xs
 
 -- p69
-filter' :: (a -> Bool) -> [a] -> [a]
-filter' _ [] = []
-filter' p (x:xs)
-  | p x       = x : filter' p xs
-  | otherwise = filter' p xs
+--filter' :: (a -> Bool) -> [a] -> [a]
+--filter' _ [] = []
+--filter' p (x:xs)
+--  | p x       = x : filter' p xs
+--  | otherwise = filter' p xs
 
 -- p70
 largestDivisible :: Integer
@@ -57,3 +57,34 @@ addThree x y z = x + y + z
 
 addThree' :: Int -> Int -> Int -> Int
 addThree' = \x -> \y -> \z -> x + y + z
+
+-- p76
+--sum' :: (Num a) => [a] -> a
+--sum' xs = foldl (\acc x -> acc + x) 0 xs
+
+-- p77
+sum' :: (Num a) => [a] -> a
+sum' = foldl (+) 0
+
+map' :: (a -> b) -> [a] -> [b]
+map' f xs = foldr (\x acc -> f x : acc) [] xs
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' y ys = foldr (\x acc -> if x == y then True else acc) False ys
+
+-- p78
+maximum' :: (Ord a) => [a] -> a
+maximum' = foldl1 max
+
+-- p79
+reverse' :: [a] -> [a]
+reverse' = foldl (flip (:)) []
+
+product' :: (Num a) => [a] -> a
+product' = foldl1 (*)
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' p = foldr (\x acc -> if p x then x : acc else acc) []
+
+last' :: [a] -> a
+last' = foldl1 (\_ x -> x)
