f5758373d8b719a32149a575689ec5032330eb98
tokoyax <tokoyax.dev@gmail.com>
2017-12-27 12:51:08 +0900

    module p95

---
+++ src/chapter6.hs
@@ -0,0 +1,10 @@
+import Data.List
+
+numUniques :: (Eq a) => [a] -> Int
+numUniques = length . nub
+
+wordNums :: String -> [(String, Int)]
+wordNums = map (\ws -> (head ws, length ws)) . group . sort . words
+
+isIn :: (Eq a) => [a] -> [a] -> Bool
+needle `isIn` haystack = any (needle `isPrefixOf`) (tails haystack)
