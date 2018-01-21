79f31069bddf4e8414389ba9c3ae9e5e6d8c3466
tokoyax <tokoyax.dev@gmail.com>
2018-01-22 07:02:01 +0900

    page 129

---
+++ src/Person.hs
@@ -0,0 +1,11 @@
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     } deriving (Eq, Show, Read)
+
+mikeD = Person {firstName = "Michael", lastName = "Diamond", age = 43}
+adRock = Person {firstName = "Adam", lastName = "Horovits", age = 41}
+mca = Person {firstName = "Adam", lastName = "Yauch", age = 44}
+mysteryDude = "Person { firstName = \"Michael\"" ++
+                     ", lastName = \"Diamond\"" ++
+                     ", age = 43 }"
