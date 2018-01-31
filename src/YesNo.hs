4f2db29286868c552a8225bb66a559e563965b2b
tokoyax <tokoyax.dev@gmail.com>
2018-01-31 17:49:12 +0900

    p 150 done

---
+++ src/YesNo.hs
@@ -0,0 +1,34 @@
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+data TrafficLight = Red | Yellow | Green
+
+class YesNo a where
+    yesno :: a -> Bool
+
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+instance YesNo [a] where
+    yesno [] = False
+    yesno _ = True
+
+instance YesNo Bool where
+    yesno = id
+
+instance YesNo (Maybe a) where
+    yesno (Just _) = True
+    yesno Nothing = False
+
+instance YesNo (Tree a) where
+    yesno EmptyTree = False
+    yesno _ = True
+
+instance YesNo TrafficLight where
+    yesno Red = False
+    yesno _ = True
+
+yesnoIf :: (YesNo y) => y -> a -> a -> a
+yesnoIf yesnoVal yesResult noResult =
+        if yesno yesnoVal
+            then yesResult
+            else noResult
