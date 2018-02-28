cdbff8d1e2d3204016afd3d1ac99234b034929b6
tokoyax <tokoyax.dev@gmail.com>
2018-02-28 23:45:59 +0900

    chapter 11 done

---
+++ src/sequenceA.hs
@@ -0,0 +1,8 @@
+import Control.Applicative
+
+sequenceA' :: (Applicative f) => [f a] -> f [a]
+sequenceA' [] = pure []
+sequenceA' (x:xs) = (:) <$> x <*> sequenceA' xs
+
+sequenceA'' :: (Applicative f) => [f a] -> f [a]
+sequenceA'' = foldr (liftA2 (:)) (pure [])
