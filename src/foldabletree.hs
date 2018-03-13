718ad0a08035e818079227bfcedc41c4555b76de
tokoyax <tokoyax.dev@gmail.com>
2018-03-13 09:32:14 +0900

    Chapter 12 done

---
+++ src/foldabletree.hs
@@ -0,0 +1,20 @@
+import qualified Data.Foldable as F
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+
+instance F.Foldable Tree where
+    foldMap f EmptyTree = mempty
+    foldMap f (Node x l r) = F.foldMap f l `mappend`
+                             f x           `mappend`
+                             F.foldMap f r
+
+
+testTree = Node 5
+              (Node 3
+                  (Node 1 EmptyTree EmptyTree)
+                  (Node 6 EmptyTree EmptyTree)
+              )
+              (Node 9
+                  (Node 8 EmptyTree EmptyTree)
+                  (Node 10 EmptyTree EmptyTree)
+              )
