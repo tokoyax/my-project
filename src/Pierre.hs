db6ea23c480342ec1df76cda069568471d017ba0
tokoyax <tokoyax.dev@gmail.com>
2018-03-20 22:19:03 +0900

    banana

--- src/Pierre.hs
+++ src/Pierre.hs
@@ -12,3 +12,18 @@ landRight n (left, right)
     | otherwise = Nothing
 
 x -: f = f x
+
+banana :: Pole -> Maybe Pole
+banana _ = Nothing
+
+routine :: Maybe Pole
+routine = case landLeft 1 (0, 0) of
+    Nothing -> Nothing
+    Just pole1 -> case landRight 4 pole1 of
+        Nothing -> Nothing
+        Just pole2 -> case landLeft 2 pole2 of
+            Nothing -> Nothing
+            Just pole3 -> landLeft 1 pole3
+
+
+
