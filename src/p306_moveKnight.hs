efc2a766922c20dbea99efa6fa657a8347cfae9a
tokoyax <tokoyax.dev@gmail.com>
2018-03-26 15:31:40 +0900

    chapter 13.6 done

--- src/p306_moveKnight.hs
+++ src/p306_moveKnight.hs
@@ -1,3 +1,5 @@
+import           Control.Monad
+
 type KnightPos = (Int, Int)
 
 moveKnight :: KnightPos -> [KnightPos]
@@ -7,3 +9,13 @@ moveKnight (c,r) = do
                 ]
     guard (c' `elem` [1..8] && r' `elem` [1..8])
     return (c', r')
+
+in3 :: KnightPos -> [KnightPos]
+in3 start = do
+    first <- moveKnight start
+    second <- moveKnight first
+    moveKnight second
+
+canReachIn3 :: KnightPos -> KnightPos -> Bool
+canReachIn3 start end = end `elem` in3 start
+
